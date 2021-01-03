6ada27cf607902ca0ba3f88eba9c22c3e1ec1892
Dustin Miller <dwmiller@anderson.edu>
2021-01-03 10:53:40 -0700

    New routes and layout

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,12 @@
 Rails.application.routes.draw do
   root 'html_pages#home'
-  get 'html_pages/home'
-  get 'html_pages/help'
-  get 'html_pages/about'
+  # get 'html_pages/home'
+  # get 'html_pages/help'
+  get 'help', to: 'html_pages#help'
+  # get 'html_pages/about'
+  get 'about', to: 'html_pages#about'
+  # get 'html_pages/contact'
+  get 'contact', to: 'html_pages#contact'
+  # now have users controller
+  get 'signup', to: 'users#new'
 end
